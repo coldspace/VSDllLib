0ef28b1be23b1eb98f17d38fd7ece6322f508ca1
zwenc <1005714267@qq.com>
2019-02-24 17:35:16 +0800

    提交dllapp，用于测试dll

---
+++ dllapp/include/dlltest.h
@@ -0,0 +1,15 @@
+#ifndef DLLTEST_H
+#define DLLTEST_H
+
+//#define dll_export
+
+#ifdef dll_export
+	#define CALL_Dll extern "C" __declspec(dllexport)  // 此时为生成dll，为保证兼容，限制以C的方式
+#else
+	#define CALL_Dll extern "C" __declspec(dllimport)  // 此时为导入dll，为保证兼容，限制以C的方式
+#endif 
+
+CALL_Dll int sum(const int x, const int y);
+CALL_Dll int sub(const int x, const int y);
+
+#endif
---
+++ dllapp/main.cpp
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "dlltest.h"
+
+void main()
+{
+	printf("1+2=%d \n", sum(1, 2));
+	printf("2-1=%d \n", sub(2, 1));
+}
